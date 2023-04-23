ac92a87b57c4f66abdc10b5b759374a4f6d2f9f6
sujalgera01 <sujalgera01@gmail.com>
2023-04-24 01:12:09 +0530

    Updates

---
+++ precache-manifest.b9c14d2c372c9d82742c5b5c89b72739.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0781f4775c65161c1f68",
+    "url": "/PridhiPortfolio/static/css/main.a615221d.chunk.css"
+  },
+  {
+    "revision": "0781f4775c65161c1f68",
+    "url": "/PridhiPortfolio/static/js/main.c1cf1ffd.chunk.js"
+  },
+  {
+    "revision": "d753283917d3446d0500",
+    "url": "/PridhiPortfolio/static/js/runtime~main.ca480218.js"
+  },
+  {
+    "revision": "34486e8adfff4ee1347f",
+    "url": "/PridhiPortfolio/static/js/2.c113e7b2.chunk.js"
+  },
+  {
+    "revision": "093b6b0195696f1ad185b4f581461881",
+    "url": "/PridhiPortfolio/static/media/PriResume.093b6b01.pdf"
+  },
+  {
+    "revision": "b5e56a1581545c9ee9d16c6f26c4e475",
+    "url": "/PridhiPortfolio/static/media/Priimage1.b5e56a15.png"
+  },
+  {
+    "revision": "93d2b7e7a4a590d959957b9e5e65d41f",
+    "url": "/PridhiPortfolio/static/media/Priimage0.93d2b7e7.jpeg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/PridhiPortfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/PridhiPortfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/PridhiPortfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/PridhiPortfolio/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/PridhiPortfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/PridhiPortfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "d8dcda7d7f4662a748f6915e1c118949",
+    "url": "/PridhiPortfolio/index.html"
+  }
+];
